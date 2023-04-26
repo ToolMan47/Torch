4048d255771b49fdcc3039f50be23c45a335787b
ToolMan47 <kh88403@gmail.com>
2023-04-26 09:12:07 +0800

    Initial commit

---
+++ src/test/java/com/toolman/torch/TorchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toolman.torch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TorchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
